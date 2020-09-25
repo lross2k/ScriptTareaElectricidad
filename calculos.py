85f5ac29741fd6182f83611d73855013280d79a3
lross2k <luisross2000@outlook.com>
2020-09-25 17:47:44 -0600

    Para leer los datos

--- calculos.py
+++ calculos.py
@@ -1,3 +1,20 @@
 import csv
 import numpy as np
 import matplotlib.pyplot as plt
+
+# Abrir y leer archivo CSV
+with open('OCV(z).csv', mode = 'r' ) as read_file:
+
+    content = csv.reader(read_file)
+    cond_first = True
+    soc = np.array([])
+    ocv = np.array([])
+
+    for row in content:
+
+        if cond_first: title = row; cond_first = False; continue
+
+        soc = np.append(soc, float(row[0]))
+        ocv = np.append(ocv, float(row[1]))
+
+    data = np.array([soc, ocv])
